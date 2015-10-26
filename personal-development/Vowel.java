36b80ccebbcb87e350772a1c6c430fdbbbe408c0
rtduany <rtduany@gmail.com>
2015-10-26 23:26:42 +0300

    add class Vowel with a method that takes in a string

---
+++ personal-development/Vowel.java
@@ -0,0 +1,11 @@
+//define a Vowel.java class
+class Vowel{
+	//lets define a static void vowelToIndex method that takes in 1 string parameter
+	static void vowelToIndex(String s){
+		//instantiate a StringBuilder object named res to append character objects
+		StringBuilder res = new StringBuilder();
+		//define a string array that holds a list of characters after the string is split with empty spaces
+		String[] words = s.split(" +");
+
+	}
+}

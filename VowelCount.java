7df7fb80144c112897958139141618da414935dd
rtduany <rtduany@gmail.com>
2015-11-11 10:28:26 +0300

    modify the program to make it run

--- VowelCount.java
+++ VowelCount.java
@@ -14,18 +14,42 @@
 // vowelMap.get('e'); // 1
 // vowelMap.get('o'); // null
 // HashMap vowelMap = VowelCount.vowelCount("do we control our computers, or do they control us?");
-// vowelMap.get('o'); // 8
+// vowelMap.get('o'); // 9
 // vowelMap.get('i'); // 3
 // vowelMap.get('e'); // 3
 // vowelMap.get('u'); // 3
 
+import java.util.HashMap;
 //define a VowelCount.java
-class VowelCount{
+public class VowelCount{
 	//define a vowelCount method for VowelCount class
-	public void vowelCount{
+	//that takes in 1 parameter
+	static HashMap vowelCount(String userString){
+		// Creating new HashMap objects
+        // keys are String, values are Integer
+		HashMap<Character, Integer> vowels = new HashMap<Character, Integer>() {
+		{
+			put('a', 0);
+			put('e', 0);
+			put('i', 0);
+			put('o', 0);
+			put('u', 0);
+		}
+	};
+	for (int count = 0; count < userString.length(); count++)
+{
+    char ch = userString.charAt(count);
+    ch = Character.toLowerCase(ch);
 
+    if (vowels.containsKey(ch)) {
+        vowels.put(ch, vowels.get(ch) + 1);
+    }
+}
+		System.out.println(vowels);
+		return vowels;
 	}
 	public static void main(String[] args){
-
+		HashMap vowelMap = VowelCount.vowelCount("do we control our computers, or do they control us?");
+		vowelMap.get('o');
 	}
 }

56f8524d7c6b58996c1761fedacd48d2c8cb2494
rtduany <rtduany@gmail.com>
2015-11-26 21:57:25 +0300

    add function and logic to Divisible.py

--- Divisible.py
+++ Divisible.py
@@ -1,1 +1,7 @@
 #Create a function isDivisible(n,...) that checks if the first #agrument n is divisible by all other arguments (return true if no #other arguments)
+def isDivisible(n,x,y,z):
+	if (n % x == 0) and (n % y == 0) and (n % z == 0):
+		return True
+	else:
+		return False
+print isDivisible(8,1,2,4)

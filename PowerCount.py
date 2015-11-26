8d36398f5b9673bee5b372fcb33f19678659a0cd
rtduany <rtduany@gmail.com>
2015-11-26 15:29:49 +0300

    add powerCount.py class

---
+++ PowerCount.py
@@ -0,0 +1,4 @@
+#create a function powers that takes an array, and returns the number #of subsets possible to create from that list. In other words, counts #the power sets.
+#For instance
+#powers([1,2,3]) =&gt; 8
+

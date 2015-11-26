f862ef1d3d5417b8feb59046191f1384b7b3be7e
rtduany <rtduany@gmail.com>
2015-11-26 04:20:09 +0300

    add the unique integer array method

--- UniqueArray.java
+++ UniqueArray.java
@@ -1,9 +1,19 @@
 //import java.util.*;
 public class UniqueArray {
 	public static int[] unique(int[] arr) {
-
+		int end = arr.length();
+		for (int i = 0; i < end; i++){
+			for (int j = i + 1; j < end; j++){
+				if (arr[i] == arr[j]){
+					arr[j] = arr[end - 1];
+					end--;
+					j--;
+				}
+			}
+		}
+		int[] emptyList = new int[end];
+		System.arraycopy(arr, 0, emptyList, 0, end);
+		return emptyList;
   }
-	public static void main(String[] args){
-		int[] a = {1, 2, 3, 9, 2, 5, 8, 4, 5};
-	}
+
   }

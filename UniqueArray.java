0d1c19ea205f8034cbf1ffa5bcd7aff08345df4d
rtduany <rtduany@gmail.com>
2015-11-26 04:09:08 +0300

    add UniqueArray.java class

---
+++ UniqueArray.java
@@ -0,0 +1,9 @@
+//import java.util.*;
+public class UniqueArray {
+	public static int[] unique(int[] arr) {
+
+  }
+	public static void main(String[] args){
+		int[] a = {1, 2, 3, 9, 2, 5, 8, 4, 5};
+	}
+  }

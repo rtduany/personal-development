ee7efc7f982a36ef447350619bf8d75e9acb50cc
rtduany <rtduany@gmail.com>
2015-11-26 10:41:31 +0300

    add BiledEggs.py file

---
+++ BoiledEggs.py
@@ -0,0 +1,14 @@
+#Implement a function, which takes a non-negative integer, #representing the number of eggs to boil. It must return the time in #minutes (integer), which it takes to have all the eggs boiled.
+
+#Rules
+
+#you can put at most 8 eggs into the pot at once
+#it takes 5 minutes to boil an egg
+#we assume, that the water is boiling all the time (no time to heat #up)
+#for simplicity we also don't consider the time it takes to put eggs #into the pot or get them out of it
+#Example
+
+#cooking_time 0 # must return 0
+#cooking_time 5 # must return 5
+#cooking_time 10 # must return 10
+

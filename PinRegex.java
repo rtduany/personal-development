4082efb2b79f021537ebad7d6d41bae2e96a1047
rtduany <rtduany@gmail.com>
2015-12-15 10:38:24 +0300

    define validate_Pin method

--- PinRegex.java
+++ PinRegex.java
@@ -3,6 +3,19 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PinRegex{
+public class PinRegex {
+	private String pin;
+	Pattern p = Pattern.compile("^[A-Z]{3}|[a-z]{3}\s*[1-9]\d{2}[a-zA-Z]{1}$");
 
+	//define a validate_Pin method that takes 1 string parameter
+	public void validate_Pin(String pin){
+		this.pin = pin;
+		// Now create matcher object.
+		Matcher m = p.matcher(pin);
+		if (m.find()){
+			System.out.println("The license plate matches!")
+		}else {
+			System.out.println("Wrong format!")
+		}
+	}
 }

4b024532b094b7ba76a59958417099aa7a50630c
rtduany <rtduany@gmail.com>
2015-12-15 11:46:42 +0300

    fix regex logic and make program work

--- PinRegex.java
+++ PinRegex.java
@@ -5,17 +5,25 @@ import java.util.regex.Pattern;
 
 public class PinRegex {
 	private String pin;
-	Pattern p = Pattern.compile("^[A-Z]{3}|[a-z]{3}\s*[1-9]\d{2}[a-zA-Z]{1}$");
+	//Pattern p = Pattern.compile("^[A-Z]{3}|[a-z]{3}\\s*[1-9]\\d{2}[a-zA-Z]{1}$");
 
 	//define a validate_Pin method that takes 1 string parameter
 	public void validate_Pin(String pin){
 		this.pin = pin;
+		Pattern p = Pattern.compile("(([A-Z]{3})|([a-z]{3}))([\\s])?([1-9][\\d]{2})(([A-Z]{1})|([a-z]{1}))$");
 		// Now create matcher object.
 		Matcher m = p.matcher(pin);
 		if (m.find()){
-			System.out.println("The license plate matches!")
+			System.out.println("The license plate matches!");
 		}else {
-			System.out.println("Wrong format!")
+			System.out.println("Wrong format!");
 		}
+	}
+	public static void main(String[] args){
+		PinRegex pr = new PinRegex();
+		pr.validate_Pin("KBA 445H");
+
+		PinRegex pr1 = new PinRegex();
+		pr1.validate_Pin("KBA 445a");
 	}
 }

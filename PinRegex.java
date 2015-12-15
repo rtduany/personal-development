ebe2bf9456b38cb4f31dcaaf35c68bd0b9005f9f
rtduany <rtduany@gmail.com>
2015-12-15 08:42:01 +0300

    add PinRegax.java class

---
+++ PinRegex.java
@@ -0,0 +1,8 @@
+//write a program that validates car license plate user input. The //plates are made of 3 letters (case-insensitive); an optional white //space; and 1 letter (case-insensitive). Use regex for validation
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PinRegex{
+
+}

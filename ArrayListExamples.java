3a17399f0118ec19707896ce272a4315cccd55bd
rtduany <rtduany@gmail.com>
2015-12-18 11:57:56 +0300

    create ArrayList and add some items to it

--- ArrayListExamples.java
+++ ArrayListExamples.java
@@ -1,5 +1,16 @@
 import java.util.*;
 //declare the ArrayListExamples.java class
 public class ArrayListExamples{
+	//define the main method for our ArrayListExamples.java class
+	public static void main(Sring[] args){
+		//create an empty String array list
+		ArrayList<String> list = new ArrayList<String>();
 
+		//add items to the list
+		list.add(item1);
+		list.add(item2);
+		list.add(item3);
+		//add item4 to the fourth position of the list
+		list.add(3, item4);
+	}
 }

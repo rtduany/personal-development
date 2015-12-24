11afbb527dc6a56272a46cee8234fe61f89826a3
rtduany <rtduany@gmail.com>
2015-12-24 13:52:58 +0300

    add CaffeineScript.java class to personal-development directory

---
+++ CaffeineScript.java
@@ -0,0 +1,15 @@
+/* Complete the function caffeineBuzz, which takes a non-zero integer as it's one argument.
+If the integer is divisible by 3, return the string "Java".
+If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+If the integer is one of the above and is even, add "Script" to the end of the string.
+Otherwise, return the string "mocha_missing!"
+
+caffeineBuzz(1)   =&gt; "mocha_missing!"
+caffeineBuzz(3)   =&gt; "Java"
+caffeineBuzz(6)   =&gt; "JavaScript"
+caffeineBuzz(12)  =&gt; "CoffeeScript"
+*/
+//create a CaffeineScript class
+public class CaffeineScript{
+
+}

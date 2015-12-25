f1a865147e2831fea4ddb6425d992b86b5ca570e
rtduany <rtduany@gmail.com>
2015-12-25 15:03:58 +0300

    define reverse method that takes 1 string parameter

--- ReverseFun.java
+++ ReverseFun.java
@@ -1,3 +1,16 @@
 public class ReverseFun {
+	public static String reverse(String input){
+		StringBuilder sb = new StringBuilder();
+		int length = input.length();
+		for (int i = 0; i < length; i++){
+			int j = i;
+			while (j < length){
+				j++;
+			}
+			sb.append(reverseString(input.substring(i, j)));
+			i = j - 1;
+		}
+		return sb.toString();
+	}
 
 }

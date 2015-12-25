73302e175d826e15aad0b29c65f1d7680b9ea8e1
rtduany <rtduany@gmail.com>
2015-12-25 03:31:21 +0300

    fix buggs

--- CaffeineScript.java
+++ CaffeineScript.java
@@ -3,13 +3,28 @@ If the integer is divisible by 3, return the string "Java".
 If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
 If the integer is one of the above and is even, add "Script" to the end of the string.
 Otherwise, return the string "mocha_missing!"
-
 caffeineBuzz(1)   =&gt; "mocha_missing!"
 caffeineBuzz(3)   =&gt; "Java"
 caffeineBuzz(6)   =&gt; "JavaScript"
 caffeineBuzz(12)  =&gt; "CoffeeScript"
 */
-//create a CaffeineScript class
 public class CaffeineScript{
+	//define a caffeineBuzz method
+	public static String caffeineBuzz(int n){
+		String message = "mocha_missing!";
+			if (n != 0){
+				if (n % 3 == 0)
+					message = "Java";
+				if (n % 12 == 0)
+					message = "Coffee";
+				if (n % 2 == 0)
+					message += "Script";
 
+			}
+		return message;
+	}
+	public static void main(String[] args){
+		String returned = caffeineBuzz(6);
+		System.out.println(returned);
+	}
 }

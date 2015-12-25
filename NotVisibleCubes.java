d2c4c817b4310f3bf82c05e7207c56bd6599777a
rtduany <rtduany@gmail.com>
2015-12-25 03:07:04 +0300

    define notVisibleCubes method that takes in 1 parameter

--- NotVisibleCubes.java
+++ NotVisibleCubes.java
@@ -1,3 +1,9 @@
 public class NotVisibleCubes {
+	//define a notVisibleCubes method that takes in 1 parameter of type Long
+	public Long notVisibleCubes(Long n) {
+		int k = 3;
+		Long inner = (n-2)*(n-2)*(n-2);
+	return inner;
+	}
 
 }

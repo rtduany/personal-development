674ea1c423f029ba1d363afec00369c666f7d661
rtduany <rtduany@gmail.com>
2015-12-25 17:38:46 +0300

    add Numbers.java to personal-development directory

---
+++ Numbers.java
@@ -0,0 +1,7 @@
+/*Each number should be formatted that only the first two decimal places are returned. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+Don't round the numbers! Just cut them after two decimal places!
+*/
+public class Numbers{
+
+}

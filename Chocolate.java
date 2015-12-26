9cffebb659e9e1310f9659367182838646db40fc
rtduany <rtduany@gmail.com>
2015-12-26 14:01:52 +0300

    define a breakChocolate method that takes in 2 parameters

--- Chocolate.java
+++ Chocolate.java
@@ -1,4 +1,12 @@
 //lets define a class called Chocolate
 public class Chocolate{
-
+	//define a breakChocolate method that takes in 2 parameters as integers
+	public static int breakChocolate(int n, int m){
+		//declare an integer minBreaks and set it equal to zero
+		int minBreaks = 0;
+		//if n and m are not zero, calculate the minBreaks
+		if (n != 0 && m != 0){
+			minBreaks = (n * m) - 1;
+		}
+	}
 }

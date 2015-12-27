287c36558764c19380f5abc1a8d1cdf8818a933a
rtduany <rtduany@gmail.com>
2015-12-27 11:08:08 +0300

    define alternateSqSum method that takes in 1 parameter

--- AlternateSqSum.java
+++ AlternateSqSum.java
@@ -4,5 +4,18 @@ Example:
 alternateSqSum(new int[] {11, 12, 13, 14, 15}) // should return 379
 */
 public class AlternateSqSum{
+	public static int alternateSqSum(int[] arr) {
+		int oddSum = 0;
+		int evenSum = 0;
+		int total = 0;
+		for (int i = 1; i < arr.length+1; i++){
+			if (i % 2 == 0)
+				oddSum = oddSum + (arr[i-1]) * (arr[i-1]);
+			if (i % 2 != 0)
+				evenSum = evenSum + arr[i-1];
+		}
+		total = oddSum + evenSum;
+		return total;
+	}
 
 }

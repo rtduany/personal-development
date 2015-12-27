275ca17f1915a945ee47e0ddc76daa4b96018ea3
rtduany <rtduany@gmail.com>
2015-12-27 11:06:57 +0300

    add AlternateSqSum class to personal-development directory

---
+++ AlternateSqSum.java
@@ -0,0 +1,8 @@
+/*
+Write a method alternate_sq_sum (JS: alternateSqSum ) that takes an array of Integers as Input and finds the sum of squares of the elements at even positions (i.e 2nd 4th 6th etc etc elements) and the rest of the elements at odd Position (i.e 1st 3rd etc elements) without squaring them (the odd ones) thus completing whole array.
+Example:
+alternateSqSum(new int[] {11, 12, 13, 14, 15}) // should return 379
+*/
+public class AlternateSqSum{
+
+}

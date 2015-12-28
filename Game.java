a804d11c04d4096bb5ed1321705d680bcfa629de
rtduany <rtduany@gmail.com>
2015-12-28 19:10:17 +0300

    add Game class to personal-development directory

---
+++ Game.java
@@ -0,0 +1,11 @@
+/* deckSteve and deckJosh are arrays representing their decks. They are filled with "cards", represented by a single character. The card rank is as follows (from lowest to highest):
+
+'2','3','4','5','6','7','8','9','T','J','Q','K','A'
+Every card may appear in the deck more than once. Figure out who is going to win and return who wins and with what score:	*/
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+
+public class Game {
+
+}

61072ad9feb7f2aea87d5b61b70c58343ab979c7
rtduany <rtduany@gmail.com>
2015-12-30 08:15:24 +0300

    add CompoundArray class to personal-development directory

---
+++ CompoundArray.java
@@ -0,0 +1,12 @@
+/*
+You have to create a method "compoundArray" which should take as input two int arrays of different length and return one int array with numbers of both arrays shuffled one by one. Example: Input - {1,2,3,4,5,6} and {9,8,7,6} Output - {1,9,2,8,3,7,4,6,5,6}
+*/
+/*
+Declare the c array with a length equal to the sum of the lengths of the two arrays. Then use System.arraycopy to copy the contents of the original arrays into the new array, being careful to copy them into the destination array at the correct start index. I would use an arraylist since the size is not permanent.
+*/
+import java.util.Arrays;
+import java.util.ArrayList;
+public class CompoundArray{
+	public static int[] compoundArray(int[] a, int[] b){
+
+}

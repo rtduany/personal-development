fd2e48ae340eea80906a52c5a4667254c21540a1
rtduany <rtduany@gmail.com>
2015-12-31 15:36:39 +0300

    define comp method that takes in 2 parameters

--- AreSame.java
+++ AreSame.java
@@ -1,5 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
 public class AreSame{
+	public static boolean comp(int[] a, int[] b){
+		if (a == null || b == null) {
+                return false;
+            }
+            ArrayList<Integer> elements = new ArrayList<>();
+            for (int elementA : a) {
+                elements.add(elementA * elementA);
+            }
+            for (int elementB : b) {
+                if (!elements.contains(elementB)) {
+                    return false;
+                }
+            }
+            return true;
+	}
 
 }

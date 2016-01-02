6896d90085a6eb0ba5cf3409409ec9cb25e1fa4e
rtduany <rtduany@gmail.com>
2016-01-02 11:15:15 +0300

    define a Rectangle method that takes 2 parameters

--- Rectangle.java
+++ Rectangle.java
@@ -1,6 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Rectangle {
+	public static List<Integer> Rectangle(int lng, int wdth) {
+		if (lng == wdth)
+		return null;
+		//declare an ArrayList to hold our result
+		List<Integer> res = new ArrayList<Integer>();
 
+		while (lng != 0 && wdth != 0) {
+			if (lng > wdth) {
+				res.add(wdth);
+				lng = lng - wdth;
+			} else {
+				res.add(lng);
+				wdth = wdth - lng;
+			}
+		}
+		return res;
+	}
 
 }

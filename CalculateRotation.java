d7566b26d41104b2430db1f333bda25101dce751
rtduany <rtduany@gmail.com>
2016-01-09 09:28:43 +0300

    add CalculateRotation class to personal-development directory

---
+++ CalculateRotation.java
@@ -0,0 +1,7 @@
+/* The goal of this exercise is to write a method that takes two strings as parameters and returns an integer n, where n is equal to the amount of spaces "rotated forward" the second string is relative to the first string (more precisely, to the first character of the first string).
+For instance, take the strings "fatigue" and "tiguefa". In this case, the first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+If the second string isn't a valid rotation of the first string, the method returns -1.
+*/
+public class CalculateRotation {
+
+}

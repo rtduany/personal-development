f8eda709268c54d4e1bacf8484ea7e2d3860f113
rtduany <rtduany@gmail.com>
2016-01-09 09:36:31 +0300

    define the main method

--- CalculateRotation.java
+++ CalculateRotation.java
@@ -22,5 +22,7 @@ public class CalculateRotation {
 	static boolean isRotation(String s1,String s2) {
 		return (s1.length() == s2.length()) && ((s1+s1).indexOf(s2) != -1);
 	}
-
+	public static void main(String[] args){
+		System.out.println("The number of forward rotations is: " + shiftedDiff("Esham", "Esham"));
+	}
 }

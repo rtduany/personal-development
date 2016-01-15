32e8a8aa209258793cb756ece8fe0fbe9172b860
rtduany <rtduany@gmail.com>
2016-01-15 14:07:29 +0300

    add DRoot class to personal-development directory

---
+++ DRoot.java
@@ -0,0 +1,9 @@
+/* In this kata, you must create a digital root function. A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers. Here's how it works (Ruby example given):
+digital_root(16)
+=> 1 + 6
+=> 7
+*/
+import java.util.ArrayList;
+public class DRoot{
+
+}

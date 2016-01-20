6896e76400e0eda2acedce0f1f1e2eccf70b2c89
rtduany <rtduany@gmail.com>
2016-01-20 14:56:29 +0300

    define a distance method that takes 4 parameters

--- Distance.java
+++ Distance.java
@@ -26,5 +26,21 @@ import static java.lang.Math.*;
 public class Distance{
 	//declare a constant field called R which is the earth's radius
 	public static final double R = 6371000;
+	//define a a distance method that takes in 4 float values as parameters
+	private static double distance(double lat_a, double lng_a, double lat_b, double lng_b) {
+		double pk = 180/3.14169;
+
+		double a1 = lat_a / pk;
+		double a2 = lng_a / pk;
+		double b1 = lat_b / pk;
+		double b2 = lng_b / pk;
+
+		double t1 = Math.cos(a1)*Math.cos(a2)*Math.cos(b1)*Math.cos(b2);
+		double t2 = Math.cos(a1)*Math.sin(a2)*Math.cos(b1)*Math.sin(b2);
+		double t3 = Math.sin(a1)*Math.sin(b1);
+		double tt = Math.acos(t1 + t2 + t3);
+
+		return R*tt;
+	}
 
 }
